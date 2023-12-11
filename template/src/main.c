6977b65ecf1ec99a459dcfffb17ade7f60e3c305
geremia <germysettesette@gmail.com>
2023-12-11 12:24:15 +0100

    template update

--- template/src/main.c
+++ template/src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_separators(int rows) {
+void print_separators(size_t rows) {
     for(int i = 0; i < rows; i++) {
         printf("----------------------------------------------------------\n");
     }
